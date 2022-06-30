7a332b0fff631364e53def30b186dfe904bd1ba7
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-30 14:59:59 -0500

    Update reducer with the pokemons and page elements.

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -4,6 +4,9 @@ export const getPokemons = ( page = 0 ) => {
     return async ( dispatch, getState ) => {
         dispatch( startLoadingPokemons() );
         // ToDo: make the http request
-        // dispatch( setPokemons());
+        const answer = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+        const data = await answer.json();
+        
+        dispatch( setPokemons({ pokemons: data.results, page: page + 1}));
     }
 }
