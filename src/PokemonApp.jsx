bdab9bc6f6781873c25933a6a9867bf3b0b25f8c
TheLuisAbarca <arclaav.adm@gmail.com>
2022-06-30 15:58:36 -0500

    Preparing everything for final implementation of Async stuff.

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -13,12 +13,20 @@ export const PokemonApp = () => {
     <>
         <h1>PokemonApp</h1>
         <hr />
+        <span>Loading: {true ? 'True': 'False'}</span>
 
         <ul>
             <li>Hola</li>
             <li>Hola</li>
             <li>Hola</li>
         </ul>
+
+        <button
+            // disabled={ isLoading }
+            // onClick={ () => dispatch( getPokemons(page)) }
+        >
+            Next
+        </button>
     </>
   )
 }
